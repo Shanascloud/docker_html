4fb713c695e20ca997c22b81d4ba1b8288cef0a6
shana <45279491+Shanascloud@users.noreply.github.com>
2022-07-29 19:11:11 +0530

    Update demo.bat

--- demo.bat
+++ demo.bat
@@ -2,5 +2,5 @@ docker stop vinod_demo1
 docker rm -f vinod_demo1
 docker image rm -f vinod_demo1
 docker build -t vinod_demo1 .
-docker run -dit --name vinod_demo1 -p 90:80 vinod_demo1
+docker run -dit --name vinod_demo1 -p 909:80 vinod_demo1
 
