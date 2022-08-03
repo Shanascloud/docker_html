aa3c02fe73effc29b1490ac9ce78a6d6ef20ed8a
shana <45279491+Shanascloud@users.noreply.github.com>
2022-08-03 19:33:22 +0530

    Update demo.bat

--- demo.bat
+++ demo.bat
@@ -1,8 +1,9 @@
-docker stop html_vinod;
-docker rm -f html_vinod;
-docker image rm -f html_vinod;
+pwd;
+docker stop html_vinod || true;
+docker rm -f html_vinod || true;
+docker image rm -f html_vinod || true;
 docker build -t html_vinod .;
-docker run -dit --name html_vinod -p 909:80 html_vinod;
-docker tag html_vinod vinosmpal/html_vinod;
-docker push vinosmpal/html_vinod
-
+docker run -dit --name html_vinod -p 80:80 html_vinod;
+docker login -u vnom1985 -p abc@12345;
+docker tag html_vinod vnom1985/html_vinod;
+docker push vnom1985/html_vinod
